3248926f4e88657d1beed8d7bfb6025c53595943
aka <aka70me@gmail.com>
2023-01-17 01:56:44 +0900

    コメント削除

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
-//vueモジュールからcreateAppメソッドを取り込み
 import { createApp } from "vue";
-//作成したコンポーネントファイルを取り込み
 import App from "./App.vue";
 
-//Vue3のオブジェクトを生成
-var app = createApp(App);
-//#appの中に組み込み
+const app = createApp(App);
 app.mount("#app");
--- webpack.config.js
+++ webpack.config.js
@@ -3,43 +3,33 @@ const { VueLoaderPlugin } = require("vue-loader");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  //モードをdevelopment、production、noneから設定(必須)
-  //development: 開発時のファイル出力モード(最適化より時間短縮、エラー表示を優先）
-  //production: 本番時のファイル出力モード（最適化されて出力）
   mode: "development",
-
-  //メインとなるjsファイル(エントリーポイント)
   entry: "./src/index.ts",
-  //ファイルの出力設定
   output: {
-    //出力先のディレクトリ（絶対パスで指定）
     path: path.resolve(__dirname, "dist"),
-    //出力ファイル名
     filename: "bundle.js",
   },
-  //ローダーの設定
   module: {
     rules: [
       {
-        test: /\.css$/, //拡張子が.cssの場合
-        use: ["vue-style-loader", "css-loader"], //vue-style-loader、css-loaderを使う
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"],
       },
       {
-        test: /\.vue$/, //拡張子が.vueの場合
-        loader: "vue-loader", //vue-loaderを使う
+        test: /\.vue$/,
+        loader: "vue-loader",
       },
       {
-        test: /\.ts$/, //拡張子が.vueの場合
-        loader: "ts-loader", //vue-loaderを使う
+        test: /\.ts$/,
+        loader: "ts-loader",
         options: { appendTsSuffixTo: [/\.vue$/] },
       },
     ],
   },
   plugins: [
-    //Vueを読み込むために必要
     new VueLoaderPlugin(),
     new HtmlWebpackPlugin({
       template: "public/index.html",
-    })
+    }),
   ],
 };
